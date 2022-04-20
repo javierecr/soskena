3cca00a86bd5fb8a7e35f6d79277859bb0bec5a9
Javier Eduardo <javierecr@gmail.com>
2022-04-20 18:03:53 -0500

    Update js for storefront password

--- assets/custom.js
+++ assets/custom.js
@@ -12,28 +12,3 @@ function ready(callback) {
 }
 
 ready(function () {});
-
-$(document).ready(function () {
-  $(window).load(function () {
-    $duration = 15;
-    $('.seconds').text($duration);
-    $('.popup-wrap').fadeIn(1500);
-
-    $myTimer = setInterval(function () {
-      startTimer();
-    }, 1000);
-    $('.popup .btn-close').on('click', function () {
-      clearInterval($myTimer);
-      $('.popup-wrap').fadeOut(500);
-    });
-
-    function startTimer() {
-      $duration--;
-      $('.seconds').text($duration);
-      if ($duration == 0) {
-        clearInterval($myTimer);
-        $('.popup-wrap').fadeOut(500);
-      }
-    }
-  });
-});
